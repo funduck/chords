5785bc22bf8ff46e5f151f8719075c58e62b5cf2
Oleg Milekhin <qlfunduck@gmail.com>
2025-08-05 20:29:15 +0400

    upd disclaimer

--- app/src/features/search/PublicSearchDisclaimer.tsx
+++ app/src/features/search/PublicSearchDisclaimer.tsx
@@ -4,7 +4,7 @@ export default function PublicSearchDisclaimer() {
   return (
     <Text c="dimmed" m="md">
       Note: The public library has just a couple of songs, so you can get a feel of the app. There are currently no
-      plans to add more songs to the public library, not least because of copyright concerns.
+      plans of adding more songs, not least because of copyright concerns.
     </Text>
   );
 }
